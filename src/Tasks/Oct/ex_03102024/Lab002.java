aed647d138c56d55cca42fa00dd10604d4f5bd55
sonalgit0997 <sonalpatel9468@gmail.com>
2024-12-27 00:22:14 +0530

    All the files added

---
+++ src/Tasks/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,10 @@
+package Tasks.Oct.ex_03102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(1+1);
+        System.out.println(2-1);
+        System.out.println(2*2);
+        System.out.println(4/2);
+    }
+}
