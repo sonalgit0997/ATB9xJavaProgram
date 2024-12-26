d9ef623a76ebfe3d91a053252427aad9ca0c0015
sonalgit0997 <sonalpatel9468@gmail.com>
2024-12-27 00:53:00 +0530

    All the files added

---
+++ src/Tasks/Oct/ex_04102024/Task003.java
@@ -0,0 +1,8 @@
+package Tasks.Oct.ex_04102024;
+
+public class Task003 {
+    public static void main(String[] args) {
+        System.out.println("All files are uploaded on Git Repository");
+        System.out.println("Task Completed");
+    }
+}
