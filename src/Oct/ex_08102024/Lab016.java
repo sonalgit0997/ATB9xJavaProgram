866fd7b29cb96cd9ace5554bf85926e786f7cf27
sonalgit0997 <sonalpatel9468@gmail.com>
2024-12-29 19:37:53 +0530

    All the files added

--- src/Oct/ex_08102024/Lab016.java
+++ src/Oct/ex_08102024/Lab016.java
@@ -3,7 +3,8 @@ package Oct.ex_08102024;
 public class Lab016 {
     public static void main(String[] args) {
         int age= 89;
-        char grades = 'A'; // characters are allowed, 'A', b, c , e, d, el , @ ,$ % ,@)(*,&,^,%,$,#,@,!)
+        char grades = 'A'; // 'A', b, c , e, d, el , @ ,$ % ,@ , ), (, *,&,^,%,$,#,@,!)
+        // characters are allowed, written in single comma ('')
         float f  = 3.14f;
         float f2 = 3.14F; //both f and F allowed
         double d = 118.9876543;
---
+++ src/Oct/ex_09102024/Lab017.java
@@ -0,0 +1,7 @@
+package Oct.ex_09102024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+
+    }
+}
