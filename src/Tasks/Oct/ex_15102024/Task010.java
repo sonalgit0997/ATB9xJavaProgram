eb4ead0d0f534be0d04fb0ca3560bcabc62cb1cb
sonalgit0997 <sonalpatel9468@gmail.com>
2025-01-01 22:46:37 +0530

    All the files added

---
+++ src/Tasks/Oct/ex_15102024/Task010.java
@@ -0,0 +1,14 @@
+package Tasks.Oct.ex_15102024;
+
+public class Task010 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+        System.out.println();
+
+        int b = 20;
+        System.out.println(--b + b++ + b--);
+        System.out.println(b);
+    }
+}
