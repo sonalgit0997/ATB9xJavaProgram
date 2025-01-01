902c754542f543547567b4de46f79ed3cc7d6b99
sonalgit0997 <sonalpatel9468@gmail.com>
2025-01-02 02:35:01 +0530

    All the files added

---
+++ src/Oct/ex_17102024_If_Conditions/Lab080_Hackkerrank2_LogicBuilding.java
@@ -0,0 +1,29 @@
+package Oct.ex_17102024_If_Conditions;
+
+public class Lab080_Hackkerrank2_LogicBuilding {
+    public static void main(String[] args) {
+
+        // ✅ Triangle Classifier:
+        //Write a program that classifies a triangle based on its side lengths.
+        //Given three input values representing the lengths of the sides,
+        // determine if the triangle is equilateral (all sides are equal),
+        // isosceles (exactly two sides are equal),
+        // or scalene (no sides are equal).
+        // Use an if-else statement to classify the triangle.
+        // side1, side2, side2 → eq, iso, scalene
+
+        // Logic Building
+        // Step 1
+        // Find the inputs / outputs
+        // Input | side1,side2,side3 -> data type -> double
+        // Output | SOP -> String or message -> Equilateral, Isosceles, Scalene.
+
+        // 2. Basic Logic |  Rough Logic
+        // if side1 == side2 and side2 == side3 -> side1 == side 3->  eq
+        // side1 == side2 || side1 == side3 || side2 == side3 -> iso
+        // else- > scalene
+
+        // Step 3
+        // - Negative Case We need Handle
+    }
+}
